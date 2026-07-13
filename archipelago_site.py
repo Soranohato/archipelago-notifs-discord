12eeb9ff25d26cc471f94a7d81e5db4f4ba6d46a
Soranohato <soranohatomusic@gmail.com>
2026-07-13 16:23:30 -0500

    Added player stat scraper

--- archipelago_site.py
+++ archipelago_site.py
@@ -19,6 +19,51 @@ def _load_tracker_site():
 def _get_hashable_key(entry):
     return (entry['Finder'],entry['Location'])
 
+def _load_player_stats_site():
+    tracker_site_ip = os.environ["PLAYER_SITE_URL"] #multiworld tracker site NOT sphere tracker
+
+    try:
+        response = requests.get(tracker_site_ip)
+        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error retrieving content from {tracker_site_ip}: {e}")
+        return None
+    
+#gets the all player stats into an array of dictionaries.
+def get_player_stats():
+    player_site_html = _load_player_stats_site()
+
+    if not player_site_html:
+        utc_now = datetime.now(timezone.utc)
+        utc_timestamp_string = utc_now.strftime("%Y-%m-%d %H:%M:%S UTC")
+
+        print(f"[{utc_timestamp_string}] [WARNING ] Failed to retrieve site!")
+        return None
+    
+    table = html.fromstring(player_site_html).find(".//table[@id='checks-table']")
+    columns = [col.text_content().strip() for col in table[0].xpath("tr/th")]
+    body = table[1]
+    foot = table[2]
+
+    result = {}
+
+    for entry in body:
+        values = [cell.text_content().strip() for cell in entry]
+        new_entry = (dict(zip(columns, values)))
+        new_entry_hash = _get_hashable_key(new_entry)
+
+        result[new_entry_hash] = new_entry
+
+    for entry in foot:
+        values = [cell.text_content().strip() for cell in entry]
+        new_entry = (dict(zip(columns, values)))
+        new_entry_hash = _get_hashable_key(new_entry)
+
+        result[new_entry_hash] = new_entry
+
+        return result
+
 # gets the n most recent actions as an array of dictionaries.
 def get_recent_archipelago_actions(n=-1):
     tracker_site_html = _load_tracker_site()
